06887dcf0c8a52c8494ef9f1b35451a390b608cc
Synaxin <129126738+Synaxin@users.noreply.github.com>
2023-04-18 11:21:40 -0600

    Add files via upload

--- scenes/assets/LevelInfo.gd
+++ scenes/assets/LevelInfo.gd
@@ -7,8 +7,11 @@ export var rotation_direction = 1
 export var base_rotation = 180
 export var hinge_rotation = 0
 
-export(StreamTexture) var overlay
-export(StreamTexture) var reference
+export var overlay = "res:///art/Levels/Overlays/"
+export var reference = "res:///art/Levels/References/"
+
+var overlay_file = null
+var ref_file = null
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -20,6 +23,7 @@ func _ready():
 	
 	pass # Replace with function body.
 	
+	
 func _get_arm_info():
 	return Vector2(base_length, hinge_length)
 
@@ -30,10 +34,14 @@ func _get_start_info():
 	return Vector2(base_rotation, hinge_rotation)
 	
 func _get_overlay():
-	return overlay
+	if overlay_file == null:
+		overlay_file = load(overlay)
+	return overlay_file
 	
 func _get_reference():
-	return reference.get_data()
+	if ref_file == null:
+		ref_file = load(reference)
+	return ref_file.get_data()
 	
 func _set_scene(base_len, hinge_len, num_rot, rot_dir, base_rot, hinge_rot, over_path, ref_path):
 	base_length = base_len
@@ -42,8 +50,8 @@ func _set_scene(base_len, hinge_len, num_rot, rot_dir, base_rot, hinge_rot, over
 	rotation_direction = rot_dir
 	base_rotation = base_rot
 	hinge_rotation = hinge_rot
-	overlay = load(over_path)
-	reference = load(ref_path)
+	overlay = over_path
+	reference = ref_path
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
